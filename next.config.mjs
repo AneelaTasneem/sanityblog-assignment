9a01f230450bfe39b661a1bbfa128e3c23b4cbc8
Aneela Tasneem <aneelatasneem90@gmail.com>
2025-01-15 01:26:15 -0800

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['cdn.sanity.io'], // Add your allowed domains here
-      },
+  images: {
+    domains: ['cdn.sanity.io'], // Add your allowed domains here
+  },
 };
 
+// Using export default for .mjs files
 export default nextConfig;
